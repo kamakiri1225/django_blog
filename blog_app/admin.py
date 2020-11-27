e04a61bf8f59221daa3856104b9ed432f47f8eb8
kamakiri1225 <36812492+kamakiri1225@users.noreply.github.com>
2020-11-27 21:40:34 +0900

    django_blog
    django_blog

---
+++ blog_app/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Blog #追加
+
+# Register your models here.
+admin.site.register(Blog) #追加
---
+++ blog_project/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path ,include
+from django.conf.urls.static import static #追加
+from django.conf import settings #追加
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('index/', include('blog_app.urls') ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
